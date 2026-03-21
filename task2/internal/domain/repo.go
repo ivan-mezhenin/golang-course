1098e6652fcc26c2b17b6688d78e24010dc3c819
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-03-21 10:52:40 +0300

    feat: domain

--- task2/internal/domain/repo.go
+++ task2/internal/domain/repo.go
@@ -1,1 +1,12 @@
 package domain
+
+import "time"
+
+type Repository struct {
+	Name        string
+	Description string
+	Stars       int
+	Forks       int
+	CreatedAt   time.Time
+	Visibility  string
+}
