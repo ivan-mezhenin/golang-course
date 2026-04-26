5436e915924e264819100d00fa48ea0eb428d361
ivan-mezhenin <mezhenin_ivan_06@mail.ru>
2026-04-26 12:15:11 +0300

    init

---
+++ task5/repo-stat/processor/cmd/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"repo-stat/platform/grpcserver"
+	"repo-stat/platform/logger"
+	"repo-stat/processor/config"
+	"repo-stat/processor/internal/adapter/collector"
+	"repo-stat/processor/internal/controller"
+	"repo-stat/processor/internal/usecase"
+	processorServer "repo-stat/proto/processor"
+)
+
+func run(ctx context.Context) error {
+
+	var configPath string
+	flag.StringVar(&configPath, "config", "config.yaml", "server configuration file")
+	flag.Parse()
+
+	cfg := config.MustLoad(configPath)
+
+	log := logger.MustMakeLogger(cfg.Logger.LogLevel)
+
+	log.Info("starting server...")
+	log.Debug("debug messages are enabled")
+
+	collectorAdapter, err := collector.NewClient(cfg.Services.Collector, log)
+	if err != nil {
+		log.Error("failed to create collector client: ", "error", err)
+		return err
+	}
+
+	repoUsecase := usecase.NewGetRepoInfo(collectorAdapter)
+
+	handler := controller.NewHandler(repoUsecase)
+
+	srv, err := grpcserver.New(cfg.GRPC.Address)
+	if err != nil {
+		return fmt.Errorf("create grpc server: %w", err)
+	}
+
+	processorServer.RegisterProcessorServer(srv.GRPC(), handler)
+
+	if err := srv.Run(ctx); err != nil {
+		return fmt.Errorf("run grpc server: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	if err := run(ctx); err != nil {
+		_, err = fmt.Fprintln(os.Stderr, err)
+		if err != nil {
+			fmt.Printf("launching server error: %s\n", err)
+		}
+		cancel()
+		os.Exit(1)
+	}
+	cancel()
+}
